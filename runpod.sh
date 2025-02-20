96d43b065b075af17794c0d38dc11cd36eb4d39e
Louis Garcia <louisgarcia@Mac.lan>
2025-02-20 15:21:18 +0100

    fixed runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+cd ScreenSpot-Pro-GUI-Grounding
+
 # Single run of the evaluation script
-python eval_screenspot_pro.py  \
+python ./eval_screenspot_pro.py  \
     --model_type "qwen2-5vl"  \
     --model_name_or_path "Qwen/Qwen2-VL-7B-Instruct"  \
     --screenspot_imgs "../data/ScreenSpot-Pro/images"  \
