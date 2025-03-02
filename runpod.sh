c6fd5b09cbf9b836e6fd6f95e277fd418f80dbc2
Louis Garcia <louisgarcia@Mac.lan>
2025-03-02 13:26:11 +0100

    fine tuned model test

--- runpod.sh
+++ runpod.sh
@@ -34,12 +34,13 @@ pip install flash-attn --no-build-isolation
 python ./download_screenspot_pro_dataset.py
 
 # Single run of the evaluation script
+#--task "all"
 python ./eval_screenspot_pro.py  \
     --model_type "qwen25vl"  \
-    --model_name_or_path "Qwen/Qwen2.5-VL-7B-Instruct"  \
+    --model_name_or_path "louisgrc/CU-Qwen-2.5-VL-7B-MacOS-Merged-V2.0"  \
     --screenspot_imgs "./screenspot_dataset/images"  \
     --screenspot_test "./screenspot_dataset/annotations"  \
-    --task "all" \
+    --task "word_macos.json, vscode_macos.json, vmware_macos.json, pycharm_macos.json, matlab_macos.json, macos_common_macos.json, excel_macos.json, davinci_macos.json, android_studio_macos.json" \
     --language "en" \
     --gt_type "positive" \
     --log_path "./results/qwen2-5vl.json" \
